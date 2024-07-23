3577faaf55052fdb5b335959d27c2b1aada55952
shrutiisuman <shrutisuman0651@gmail.com>
2024-07-23 21:13:00 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".sometext", {
+    strings: ["Web Development","Machine Learning","Data Science"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
